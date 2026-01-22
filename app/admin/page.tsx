2b046d3ec7785b1357a12873b2c52a83368b91aa
IDOEFRAIM <idoefraimtanka@gmail.com>
2026-01-22 08:00:59 +0100

    no_complete_activity

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-     health test de l app TRAVEL_EXPRESS
+     health test de l app TRAVEL_EXPRESS_chine_france
     </div>
   )
 }
